1a1db9cd023696031f423eba8769e94d11b2522f
Maciej Szczypka <szczypka.maciek@gmail.com>
2018-12-03 21:00:11 +0100

    wersja koncowa

---
+++ precache-manifest.7ee39b86361ec5952ae126a33ba92b73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "58dc5c1fe28ee78964ac",
+    "url": "/static/css/main.cfe64ca0.chunk.css"
+  },
+  {
+    "revision": "58dc5c1fe28ee78964ac",
+    "url": "/static/js/main.58dc5c1f.chunk.js"
+  },
+  {
+    "revision": "651bd5ad258badb96578",
+    "url": "/static/css/1.1f6222f7.chunk.css"
+  },
+  {
+    "revision": "651bd5ad258badb96578",
+    "url": "/static/js/1.651bd5ad.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "479f5dce2f6dc82816ece391b6165c35",
+    "url": "/index.html"
+  }
+];
